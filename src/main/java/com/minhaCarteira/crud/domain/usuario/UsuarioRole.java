8213c0808cef59002290b7e05c5529eb7be9a3aa
pereiraR3 <anthony_rodriguespereira@outlook.com>
2025-08-30 19:53:23 -0400

    fix: changing example of enum UsuarioRole

--- src/main/java/com/minhaCarteira/crud/domain/usuario/UsuarioRole.java
+++ src/main/java/com/minhaCarteira/crud/domain/usuario/UsuarioRole.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Funções de usuário no sistema")
 public enum UsuarioRole {
 
-    @Schema(description = "Visitante", example = "VISITANTE")
+    @Schema(description = "Visitante", example = "ROLE_VISITANTE")
     ROLE_VISITANTE,
 
     @Schema(description = "Administrador", example = "ROLE_ADMIN")
