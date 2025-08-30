a9fe2a22cd386e566dddd7980cbdd496117b63b9
pereiraR3 <anthony_rodriguespereira@outlook.com>
2025-08-30 19:04:17 -0400

    feat: create config CorsConfig

---
+++ src/main/java/com/minhaCarteira/crud/infra/security/cors/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.minhaCarteira.crud.infra.security.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${api.allowed.origins}")
+    private String[] origins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost:4200")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
+
