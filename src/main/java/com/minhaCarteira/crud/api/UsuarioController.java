59efc6d1fb330a26ca87c7aa6f8ee07fc7b9c22a
pereiraR3 <anthony_rodriguespereira@outlook.com>
2025-08-30 19:22:27 -0400

    fix: change name in requestMapping

--- src/main/java/com/minhaCarteira/crud/api/UsuarioController.java
+++ src/main/java/com/minhaCarteira/crud/api/UsuarioController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("api/usuario")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
