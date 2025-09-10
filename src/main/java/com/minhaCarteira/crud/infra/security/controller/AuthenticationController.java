a6a7ae458daf89755cb73d360cb444a426a7dde0
pereiraR3 <anthony_rodriguespereira@outlook.com>
2025-09-10 11:06:31 -0400

    refactor: changed endpoints to use keyclock

--- src/main/java/com/minhaCarteira/crud/infra/security/controller/AuthenticationController.java
+++ src/main/java/com/minhaCarteira/crud/infra/security/controller/AuthenticationController.java
@@ -2,29 +2,42 @@ package com.minhaCarteira.crud.infra.security.controller;
 
 import com.minhaCarteira.crud.domain.refreshToken.DTOs.RefreshTokenRequestDTO;
 import com.minhaCarteira.crud.domain.usuario.DTOs.UsuarioLoginDTO;
-import com.minhaCarteira.crud.infra.security.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthenticationController {
 
+    @Value("${keycloak.admin.client-id}")
+    private String clientId;
+
+    @Value("${keycloak.user.grant-type}")
+    private String grantType;
+
+    @Value("${keycloak.admin.client-secret}")
+    private String clientSecret;
+
+    @Value("${keyclock_url}")
+    private String keyClockUrl;
+
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
-    private final AuthenticationService authenticationService;
-
-    public AuthenticationController(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
+    public AuthenticationController() {
     }
 
     @Operation(
@@ -32,17 +45,24 @@ public class AuthenticationController {
             description = "Autentica um usuário com o email + senha, retornando um token de acesso e um refresh token"
     )
     @PostMapping("/authenticate")
-    public ResponseEntity<Map<String,String>> authenticate(@Valid @RequestBody UsuarioLoginDTO usuarioLoginDTO)
+    public ResponseEntity<String> authenticate(@Valid @RequestBody UsuarioLoginDTO usuarioLoginDTO)
     {
-        logger.info("Tentativa de autenticação para usuário: {}", usuarioLoginDTO.email());
-        try {
-            Map<String, String> tokens = authenticationService.authenticate(usuarioLoginDTO);
-            logger.info("Autenticação bem-sucedida para usuário: {}", usuarioLoginDTO.email());
-            return ResponseEntity.ok(tokens);
-        } catch (Exception e) {
-            logger.error("Falha na autenticação para usuário: {}. Erro: {}", usuarioLoginDTO.email(), e.getMessage());
-            throw e;
-        }
+        HttpHeaders headers = new HttpHeaders();
+        RestTemplate rt = new RestTemplate();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("client_id", clientId);
+        formData.add("username", usuarioLoginDTO.email());
+        formData.add("password", usuarioLoginDTO.senha());
+        formData.add("grant_type", grantType);
+        formData.add("client_secret", clientSecret);
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(formData, headers);
+
+        var result = rt.postForEntity(keyClockUrl, entity, String.class);
+
+        return result;
     }
 
     @Operation(
@@ -52,16 +72,23 @@ public class AuthenticationController {
     @PostMapping("/refresh")
     public ResponseEntity<String> refresh(@Valid @RequestBody RefreshTokenRequestDTO refreshTokenRequestDTO)
     {
-        logger.info("Solicitação de refresh token recebida");
 
-        try {
-            String newAccessToken = authenticationService.refresh(refreshTokenRequestDTO.refreshToken());
-            logger.info("Novo access token gerado com sucesso.");
-            return ResponseEntity.ok(newAccessToken);
-        } catch (Exception e) {
-            logger.error("Erro ao renovar access token: {}", e.getMessage());
-            throw e;
-        }
+        HttpHeaders headers = new HttpHeaders();
+        RestTemplate rt = new RestTemplate();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("client_id", clientId);
+        formData.add("grant_type", grantType);
+        formData.add("client_secret", clientSecret);
+        formData.add("refresh_token", refreshTokenRequestDTO.refreshToken());
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(formData, headers);
+
+        var result = rt.postForEntity(keyClockUrl, entity, String.class);
+
+        return result;
+
     }
 
 }
